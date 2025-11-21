9094d4e6995063435cd754135ea30cd802865c4f
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2025-11-21 20:50:07 +0530

    Subarray

---
+++ Arrays/SubArray/Subarray Sum Equals K.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer,Integer> mp = new HashMap<>();
+        mp.put(0,mp.getOrDefault(0,0)+1);
+        int preSum=0,cnt=0;
+
+        for(int i=0;i<nums.length;i++){
+            preSum+=nums[i];
+            int remove = preSum-k;
+            cnt+=mp.getOrDefault(remove,0);
+            mp.put(preSum,mp.getOrDefault(preSum,0)+1);
+        }
+        return cnt;
+    }
+}
