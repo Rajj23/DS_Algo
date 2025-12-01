b90f0c7a1cd4987b14b66dfdc69f7e3148b85a49
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2025-12-01 08:48:30 +0530

    Heap

---
+++ Heap/Kth Largest Element in an Array.java
@@ -0,0 +1,16 @@
+// T.C: O(log*n + n)
+// S.C: O(n)
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+
+        for(int num:nums){
+            pq.add(num);
+        }
+
+        for(int i=0;i<k-1;i++){
+            pq.poll();
+        }
+        return pq.poll();
+    }
+}
