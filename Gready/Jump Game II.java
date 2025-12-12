773f1556963cd677c9d94ba833d357b643b0c684
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2025-12-12 10:24:43 +0530

    Gready

---
+++ Gready/Jump Game II.java
@@ -0,0 +1,22 @@
+// ***************************************************************** JAVA **************************************************************************
+
+// T.C: O(n)
+// S.C: O(1)
+class Solution {
+    public int jump(int[] nums) {
+        int jumps = 0;
+        int n = nums.length;
+        int l=0,r=0;
+
+        while(r<n-1){
+            int farthest = 0;
+            for(int i=l;i<=r;i++){
+                farthest = Math.max(farthest,i+nums[i]);
+            }
+            l = r+1;
+            r = farthest;
+            jumps++;
+        }
+        return jumps;
+    }
+}
