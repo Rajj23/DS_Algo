0f4b81c13820eab525a588e2bf52896e5e14d5b1
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2025-12-20 22:49:04 +0530

    String and Gready

--- Gready/Candy.java
+++ Gready/Candy.java
@@ -75,3 +75,41 @@ class Solution {
         return result;
     }
 }
+
+// Approach 3
+// T.C: O(n)
+// S.C: O(1)
+class Solution {
+    public int candy(int[] ratings){
+        int n = ratings.length;
+        int sum = 1;
+        int i = 1;
+
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                sum++;
+                i++;
+                continue;
+            }
+
+            int peak = 1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peak++;
+                sum+=peak;
+                i++;
+            }
+
+            int down = 1;
+            while(i<n && ratings[i]<ratings[i-1]){
+                sum+= down;
+                down++;
+                i++;
+            }
+
+            if(down>peak){
+                sum+= (down - peak);
+            }
+        }
+        return sum;
+    }
+}
---
+++ String/Delete Columns to Make Sorted.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int count = 0;
+        int rows = strs.length;
+        int cols = strs[0].length();
+
+        for(int c=0;c<cols;c++){
+            for(int r=1;r<rows;r++){
+                if(strs[r-1].charAt(c) > strs[r].charAt(c)){
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
