2512917441255fa36e59448e780df41e98ab0fc9
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2025-12-24 23:09:53 +0530

    Added new appraoch

--- Arrays/Easy/Apple Redistribution into Boxes.java
+++ Arrays/Easy/Apple Redistribution into Boxes.java
@@ -1,3 +1,4 @@
+// Approach: 1
 // T.C: O(nlogn + m)
 // S.C: O(1)
 class Solution {
@@ -18,4 +19,36 @@ class Solution {
         }
         return n;
     }
+}
+
+// Approach: 2
+//T.C : O(n + m)
+//S.C : O(1)
+
+class Solution {
+public int minimumBoxes(int[] apple, int[] capacity) {
+    int totalApple = 0;
+    for(int a:apple){
+        totalApple+=a;
+    }
+
+    int[] freq = new int[51];
+    for(int cap:capacity){
+        freq[cap]++;
+    }
+
+    int count = 0;
+
+    int idx = 0;
+    for(int cap=50;cap>=1 && totalApple>0;cap--){
+        while(freq[cap] > 0 && totalApple > 0){
+            totalApple -= cap;
+            freq[cap]--;
+            count++;
+        }
+    }
+
+
+    return count;
+}
 }
