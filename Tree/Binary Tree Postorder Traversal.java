471f9c64595a36ec64b6bbad65560f121a5b24cf
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-01-09 23:50:30 +0530

    Binary Tree Postorder Traversal

--- Tree/Binary Tree Postorder Traversal.java
+++ Tree/Binary Tree Postorder Traversal.java
@@ -1,20 +1,3 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-
-
 // Approach 1(recursive)
 // T.C: O(n)
 // S.C: O(n)
