6afcdfa9501e2bba85c21e1f59d64bf5c2cde358
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-01-21 19:27:49 +0530

    Long Pressed Name

---
+++ Arrays/Easy/Long Pressed Name.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        int i = 0;
+        int j = 0;
+        int n = name.length();
+        int m = typed.length();
+
+        if(n > m) return false;
+
+        while(i < n && j < m){
+            if(name.charAt(i) == typed.charAt(j)){
+                i++;
+                j++;
+            }
+            else{
+                if(j > 0 && typed.charAt(j) == typed.charAt(j-1)){
+                    j++;
+                }else{
+                    return false;
+                }
+            }
+        }
+        
+        if (i < n) return false;
+
+        while(j < m){
+            if(typed.charAt(j) != name.charAt(n-1)){
+                return false;
+            }
+            j++;
+        }
+        return true;
+    }
+}
