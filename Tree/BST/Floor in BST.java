2df7f35d9668726bd4d899634b82523c4bbf316d
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-01-22 07:20:50 +0530

    Floor in BST

---
+++ Tree/BST/Floor in BST.java
@@ -0,0 +1,22 @@
+// T.C: O(log(n))
+// S.C: O(1)
+class Solution {
+    public static int floor(Node root, int x) {
+        // code here
+        int floor = -1;
+        
+        while(root != null){
+            if(root.data == x){
+                return root.data;
+            }
+            else if(root.data > x){
+                root = root.left;
+            }
+            else{
+                floor = root.data;
+                root = root.right;
+            }
+        }
+        return floor;
+    }
+}
