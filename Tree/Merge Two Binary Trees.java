aa1aa09679786a446117d626ffdfeae68af7db0a
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-02-17 22:51:51 +0530

    Merge Two Binary Trees

---
+++ Tree/Merge Two Binary Trees.java
@@ -0,0 +1,16 @@
+// T.C: O(n)
+// S.C: O(1)
+class Solution {
+    
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1 == null || root2 == null){
+            return root1 == null ? root2 : root1;
+        }
+        TreeNode node = new TreeNode(root1.val + root2.val);
+
+        node.left =  mergeTrees(root1.left, root2.left);
+        node.right = mergeTrees(root1.right, root2.right);
+        
+        return node;
+    }
+}
