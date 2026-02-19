962bb8ebdee6f4b4a235f5637e1ef611d1b74e7d
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-02-20 00:44:18 +0530

    Count Binary Substrings

---
+++ String/Easy/Count Binary Substrings.java
@@ -0,0 +1,23 @@
+// T.C: O(n)
+// S.C: O(1)
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int result = 0;
+
+        int curr = 1;
+        int prev = 0;
+
+        for(int i = 1; i < s.length(); i++){
+            if(s.charAt(i) == s.charAt(i-1)){
+                curr++;
+            }
+            else{
+                result += Math.min(prev, curr);
+                prev = curr;
+                curr = 1;
+            }
+        }
+
+        return result + Math.min(prev, curr);
+    }
+}
