db7d75d9c90e64179b846db95d722eadbef3f72f
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-02-25 22:44:34 +0530

    Minimum Spanning Tree

--- Graph/Minimum Spanning Tree.java
+++ Graph/Minimum Spanning Tree.java
@@ -1,3 +1,4 @@
+// Approach: Prims Algo
 // T.C: O(Elog(E))
 // S.C: O(E)
 class Solution {
@@ -45,5 +46,88 @@ class Solution {
         }
         
         return sum;
+    }
+}
+
+
+
+
+
+// Approach: Kruskal Algo
+// T.C: O(E log E)
+// S.C: O(E + V)
+class Solution {
+    
+    int[] parent;
+    int[] rank;
+    
+    int find(int x){
+        if(parent[x] == x) return x;
+        
+        return parent[x] = find(parent[x]);
+    }
+    
+    void Union(int x, int y){
+        int x_parent = parent[x];
+        int y_parent = parent[y];
+        
+        if(x_parent == y_parent) return;
+        
+        
+        if(rank[x_parent] > rank[y_parent]){
+            parent[y_parent] = x_parent;
+        }
+        else if(rank[x_parent] < rank[y_parent]){
+            parent[x_parent] = y_parent;
+        }
+        else{
+            parent[y_parent] = x_parent;
+            rank[x_parent]++;
+        }
+    }
+    
+    int Kruskal(List<int[]> adj){
+        int sum = 0;
+        
+        for(int[] temp : adj){
+            int u = temp[0];
+            int v = temp[1];
+            int wt = temp[2];
+            
+            int parent_u = find(u);
+            int parent_v = find(v);
+            
+            if(parent_u != parent_v){
+                Union(u, v);
+                sum += wt;
+            }
+        }
+        
+        return sum;
+    }
+    
+    public int spanningTree(int V, int[][] edges) {
+        // code here
+        parent = new int[V];
+        rank = new int[V];
+        
+        for(int i = 0; i < V; i++){
+            parent[i] = i;
+        }
+        
+        List<int[]> adj = new ArrayList<>();
+        
+        
+        for(int[] e : edges){
+            int u = e[0];
+            int v = e[1];
+            int w = e[2];
+            
+            adj.add(new int[]{u, v, w});
+        }
+        
+        Collections.sort(adj, (a, b)-> Integer.compare(a[2], b[2]));
+        
+        return Kruskal(adj);
     }
 }
