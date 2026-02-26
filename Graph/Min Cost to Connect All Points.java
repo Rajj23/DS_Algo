b07d789ed0838f6a4baba551306e12452d6b2324
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-02-26 23:54:40 +0530

    Prim's Algorithm Added

--- Graph/Min Cost to Connect All Points.java
+++ Graph/Min Cost to Connect All Points.java
@@ -52,3 +52,90 @@ class Solution {
         return PrismAlgo(adj, n);
     }
 }
+
+
+
+
+//Approach-2 (Using 2-D matrix graph) - Prim's Algorithm
+class Solution {
+    int[] parent;
+    int[] rank;
+
+    int find(int x){
+        if(parent[x] == x){
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+
+    void Union(int x, int y){
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if(x_parent == y_parent) return;
+
+        if(rank[x_parent] > rank[y_parent]){
+            parent[y_parent] = x_parent;
+        }
+        else if(rank[x_parent] < rank[y_parent]){
+            parent[x_parent] = y_parent;
+        }
+        else{
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+    }
+
+    int Kruskal(List<int[]> vec){
+        int sum = 0;
+
+        for(int[] curr : vec){
+            int u = curr[0];
+            int v = curr[1];
+            int wt = curr[2];
+
+            int parent_u = find(u);
+            int parent_v = find(v);
+
+            if(parent_u != parent_v){
+                Union(u, v);
+                sum += wt;
+            }
+        }
+
+        return sum;
+    }
+
+    public int minCostConnectPoints(int[][] points) {
+        int V = points.length;
+
+        parent = new int[V];
+        rank = new int[V];
+
+        for(int i = 0; i < V; i++){
+            parent[i] = i;
+        }
+
+        List<int[]> vec = new ArrayList<>();
+
+        for(int i = 0; i < V; i++){
+            for(int j = 0; j < V; j++){
+                
+                int x1 = points[i][0];
+                int x2 = points[j][0];
+
+                int y1 = points[i][1];
+                int y2 = points[j][1];
+
+                int d = Math.abs(x1-x2) + Math.abs(y1-y2);
+
+                vec.add(new int[]{i, j, d});
+            }
+        }
+
+        Collections.sort(vec, (a, b)-> Integer.compare(a[2], b[2]));
+
+        return Kruskal(vec);
+    }
+}
