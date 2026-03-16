1d383c40344d935af0c6cd848357ef393c343e22
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-03-16 08:34:03 +0530

    fit: climbing stairs add new approach

--- DP/Climbing Stairs.java
+++ DP/Climbing Stairs.java
@@ -44,3 +44,27 @@ class Solution {
         return t[n];
     }
 }
+
+
+// Approach : Enhanced of approach 2
+// T.C : O(n)
+// S.C : O(1)
+class Solution {
+    public int climbStairs(int n) {
+        if(n == 0 || n == 1 || n == 2) return n;
+
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        for(int i = 3; i <= n; i++){
+            c = a + b;
+
+            int temp = b;
+            b = c;
+            a = temp;
+        }
+
+        return c;
+    }
+}
