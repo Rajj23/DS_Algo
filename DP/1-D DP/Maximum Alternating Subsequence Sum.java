c3ddda155041a1f539a67ebf8a74c02ee4d653fd
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-03-20 08:50:27 +0530

    add bottom up code

--- DP/1-D DP/Maximum Alternating Subsequence Sum.java
+++ DP/1-D DP/Maximum Alternating Subsequence Sum.java
@@ -31,3 +31,51 @@ class Solution {
         return solve(nums, 0, 1);
     }
 }
+
+// Approach 2: bottom up
+// T.C: O(n)
+// S.C: O(n)
+class Solution {
+    public long maxAlternatingSum(int[] nums) {
+        int n = nums.length;
+        long[][] t = new long[n+1][2];
+
+        for(int idx = n-1; idx >= 0; idx--){
+            for(int isEven = 0; isEven <= 1; isEven++){
+                
+                long skip = t[idx+1][isEven];
+
+                int num = nums[idx];
+                if(isEven != 1){
+                    num *= -1;
+                }
+
+                long take = t[idx+1][1 - isEven] + num;
+
+                t[idx][isEven] = Math.max(skip, take);
+            }
+        }
+        return t[0][1];
+    }
+}
+
+
+// Approach 3: bottom up
+// T.C: O(n)
+// S.C: O(n)
+class Solution {
+    public long maxAlternatingSum(int[] nums) {
+        int n = nums.length;
+
+        long[][] t = new long[n+1][2];
+
+        for(int i = 1; i < n+1; i ++){
+
+            t[i][0] = Math.max(t[i-1][1] - nums[i-1], t[i-1][0]);
+
+            t[i][1] = Math.max(t[i-1][0] + nums[i-1], t[i-1][1]);
+        }
+
+        return Math.max(t[n][0], t[n][1]);
+    }
+}
