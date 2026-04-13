bf718762397df89633ca098601ea299748c8028d
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-04-13 23:51:48 +0530

    Minimum Distance to the Target Element

---
+++ Arrays/Easy/Minimum Distance to the Target Element.java
@@ -0,0 +1,15 @@
+// T.C: O(n)
+// S.C: O(1)
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int result = Integer.MAX_VALUE;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == target){
+                result = Math.min(result, Math.abs(i - start));
+            }
+        }
+
+        return result;
+    }
+}
