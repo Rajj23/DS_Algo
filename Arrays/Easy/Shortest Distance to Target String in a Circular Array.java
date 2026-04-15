360a1b87b251d78ef18b1718c1b00fafd9bba43b
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-04-16 00:35:26 +0530

    Shortest Distance to Target String in a Circular Array

---
+++ Arrays/Easy/Shortest Distance to Target String in a Circular Array.java
@@ -0,0 +1,18 @@
+// T.C: O(n)
+// S.C: O(1)
+class Solution {
+    public int closestTarget(String[] words, String target, int startIndex) {
+        int result = Integer.MAX_VALUE;
+        int n = words.length;
+
+        for(int i = 0; i < n; i++){
+            if(target.equals(words[i])){
+                int dist = Math.abs(startIndex - i);
+                int diff = Math.min(dist, n-dist);
+                result = Math.min(result, diff);
+            }
+
+        }
+        return result == Integer.MAX_VALUE ? -1 : result;
+    }
+}
