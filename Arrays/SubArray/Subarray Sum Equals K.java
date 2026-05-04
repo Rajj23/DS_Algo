777a336d9bfb8fcdd99f372573795ed178e2a12b
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-05-04 18:03:28 +0530

    new approach add

--- Arrays/SubArray/Subarray Sum Equals K.java
+++ Arrays/SubArray/Subarray Sum Equals K.java
@@ -1,3 +1,26 @@
+// Approach: Brute Force
+// T.C: O(n*n)
+// S.C: O(1)
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int cnt = 0;
+        for(int i = 0; i < n; i++){
+            int sum = 0;
+            for(int j = i; j < n; j++){
+                sum += nums[j];
+                if(sum == k){
+                    cnt++;
+                }
+            }
+        }
+        
+        return cnt;
+    }
+}
+
+
+// Approach: Prefix sum + HashMap
 // T.C : O(N)
 // S.C : O(N)
 class Solution {
