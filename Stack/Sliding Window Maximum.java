274bc550ca3b2194c5ae2c02f20e702bae27b6c8
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-05-04 18:52:42 +0530

    new approach add

--- Stack/Sliding Window Maximum.java
+++ Stack/Sliding Window Maximum.java
@@ -1,3 +1,35 @@
+// Approach: Using HashMap
+// T.C: O(nlogk)
+// S.C: O(k)
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        TreeMap<Integer, Integer> mp = new TreeMap<>();
+
+        int i = 0, j = 0;
+        while(j < n && j < k){
+            mp.put(nums[j], mp.getOrDefault(nums[j], 0)+1);
+            j++;
+        }
+
+        int[] result = new int[n-k+1];
+
+        while(j < n){
+            result[i] = mp.lastKey();
+            mp.put(nums[i], mp.get(nums[i])-1);
+            if(mp.get(nums[i]) == 0){
+                mp.remove(nums[i]);
+            }
+            mp.put(nums[j], mp.getOrDefault(nums[j], 0)+1);
+            i++;
+            j++;
+        }
+        result[i] = mp.lastKey();
+        return result;
+    }
+}
+
+// Approach: Using Deque
 // T.C: O(2n)
 // S.C: O(n)
 class Solution {
