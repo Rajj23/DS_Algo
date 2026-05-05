dd6fabfd79afb258fc3fb3bb6c7811e1e4b60533
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-05-05 20:56:09 +0530

    new approach

--- Stack/Implement Queue using Stacks.java
+++ Stack/Implement Queue using Stacks.java
@@ -1,3 +1,4 @@
+// Approach: Transfer all element
 // T.C: O(2n)
 // S.C: O(2n)
 class MyQueue {
@@ -46,5 +47,43 @@ class MyQueue {
     
     public boolean empty() {
         return st1.isEmpty();
+    }
+}
+
+// Approach: Lazzy Stack
+// T.C: O(2n)
+// S.C: O(2n)
+class MyQueue {
+    Stack<Integer> st1;
+    Stack<Integer> st2;
+    public MyQueue() {
+        st1 = new Stack<>();
+        st2 = new Stack<>();
+    }
+    
+    public void push(int x) {
+        st1.push(x);
+    }
+    
+    public int pop() {
+        if(st2.isEmpty()){
+            while(!st1.isEmpty()){
+                st2.push(st1.pop());
+            }
+        }
+
+        return st2.pop();
+    }
+    
+    public int peek() {
+        if(st2.isEmpty()){
+            while(!st1.isEmpty())
+                st2.push(st1.pop());
+        }
+        return st2.peek();
+    }
+    
+    public boolean empty() {
+        return st1.isEmpty() && st2.isEmpty();
     }
 }
