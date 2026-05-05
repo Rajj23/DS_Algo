03b9ef1c0e5e6f5107c7a5a9aab68761f7438016
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-05-05 09:04:21 +0530

    new approach add

--- DP/Unique Paths II.java
+++ DP/Unique Paths II.java
@@ -1,3 +1,35 @@
+// Approach: Top down
+// T.C: O(n*m)
+// S.C: O(n*m)
+class Solution {
+    int m,n;
+    int[][] t;
+    int solve(int i, int j, int[][] grid){
+        if(i == m-1 && j == n-1){
+            return grid[i][j] != 1 ? 1 : 0;
+        }
+        if(i >= m || i < 0 || j >= n || j < 0 || grid[i][j] == 1) return 0;
+
+        if(t[i][j] != -1) return t[i][j];
+
+        int down = solve(i+1, j, grid);
+        int right = solve(i, j+1, grid);
+
+        return t[i][j] = down + right;
+    }
+    public int uniquePathsWithObstacles(int[][] grid) {
+        m = grid.length;
+        n = grid[0].length;
+
+        t = new int[m][n];
+        for(int i = 0; i < m; i++){
+            Arrays.fill(t[i], -1);
+        }
+        return solve(0, 0, grid);
+    }
+}
+
+
 // Approach: Tabulization
 // T.C: O(n*m)
 // S.C: O(n*m)
