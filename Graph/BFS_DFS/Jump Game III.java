71702c5878c5cc7f2fd7d8b1538d5b048d1264f0
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-05-18 00:35:13 +0530

    new approach add

--- Graph/BFS_DFS/Jump Game III.java
+++ Graph/BFS_DFS/Jump Game III.java
@@ -1,3 +1,4 @@
+// Approach: BFS
 // T.C: O(n)
 // S.C: O(n)
 class Solution {
@@ -28,5 +29,58 @@ class Solution {
             }
         }        
         return false;
+    }
+}
+
+// Approach: Optimised BFS
+// T.C: O(n)
+// S.C: O(1)
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        int n = arr.length;
+
+        Queue<Integer> que = new ArrayDeque<>();
+        que.add(start);
+
+        while(!que.isEmpty()){
+            int idx = que.poll();
+
+            if(arr[idx] == 0) return true;
+
+            arr[idx] *= -1;
+
+            int right = idx + arr[idx];
+            if(right < n && arr[right] >= 0){
+                que.add(right);
+            }
+
+            int left = idx - arr[idx];
+            if(left >= 0 && arr[left] >= 0){
+                que.add(left);
+            }
+        }        
+        return false;
+    }
+}
+
+
+// Approach: DFS
+// T.C: O(n)
+// S.C: O(1)
+class Solution {
+    int n;
+    boolean dfs(int[] arr, int i){
+        if(i < 0 || i >= n || arr[i] < 0) return false;
+
+        if(arr[i] == 0) return true;
+
+        arr[i] *= -1;
+
+        return dfs(arr, i-arr[i]) || dfs(arr, i+arr[i]);
+    }
+    public boolean canReach(int[] arr, int start) {
+        n = arr.length;
+        
+        return dfs(arr, start);
     }
 }
