9bd4d4ceed1458160057bc228fd49c962b343719
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-05-28 23:57:23 +0530

    Maximum Subarray

---
+++ Gready/Maximum Subarray.java
@@ -0,0 +1,18 @@
+// T.C: O(n)
+// S.C: O(1)
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int ans = nums[0];
+        int sum = nums[0];
+
+        for(int i = 1; i < n; i++){
+
+            sum = Math.max(sum + nums[i], nums[i]);
+
+            ans = Math.max(sum, ans);
+        }
+
+        return ans;
+    }
+}
