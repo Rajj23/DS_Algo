d6969b6968039f0162df3335fcaed452f3f71090
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-05-29 01:28:08 +0530

    Valid Anagram

---
+++ String/Easy/Valid Anagram.java
@@ -0,0 +1,21 @@
+// T.C: O(n)
+// S.C: O(1)
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] freq = new int[26];
+
+        for(char c : s.toCharArray()){
+            freq[c-'a']++;
+        }
+
+        for(char c : t.toCharArray()){
+            freq[c-'a']--;
+        }
+
+        for(int i = 0; i < 26; i++){
+            if(freq[i] != 0) return false;
+        }
+
+        return true;
+    }
+}
