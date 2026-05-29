0fdec7c460731754551c5be8159cc85c377c76f5
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-05-29 23:56:25 +0530

    Rotate Array

---
+++ Arrays/Rotate Array.java
@@ -0,0 +1,22 @@
+// T.C: O(2n)
+// S.C: O(1)
+class Solution {
+    void rev(int start, int end, int[] nums){
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+
+        k %= n;
+
+        rev(0, n-1, nums);
+        rev(0, k-1, nums);
+        rev(k, n-1, nums);
+    }
+}
