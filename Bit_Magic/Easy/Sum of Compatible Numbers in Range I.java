f5af74815121bfb25b3eea6abf4068027518d1e4
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-06-08 01:08:18 +0530

    Sum of Compatible Numbers in Range I

---
+++ Bit_Magic/Easy/Sum of Compatible Numbers in Range I.java
@@ -0,0 +1,14 @@
+// T.C: O(n-k)
+// S.C: O(1)
+class Solution {
+    public int sumOfGoodIntegers(int n, int k) {
+        int sum = 0;
+
+        for(int x = Math.max(1, n-k); x <= n+k; x++){
+            if((n&x) == 0){
+                sum += x;
+            }
+        }
+        return sum;
+    }
+}
