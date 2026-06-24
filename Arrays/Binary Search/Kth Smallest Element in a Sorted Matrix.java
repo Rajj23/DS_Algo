1f3f9c5fa7bdee5dc4d235df4c6605e6804f1f53
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-06-24 23:26:40 +0530

    Kth Smallest Element in a Sorted Matrix

---
+++ Arrays/Binary Search/Kth Smallest Element in a Sorted Matrix.java
@@ -0,0 +1,33 @@
+// T.C: O(m*n*logn)
+// S.C: O(1)
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+
+        int l = matrix[0][0];
+        int r = matrix[n-1][n-1];
+
+        while(l <= r){
+            int mid = l + (r - l)/2;
+            int count = 0;
+
+            for(int row = 0; row < n; row++){
+                int col = n -1;
+                while(col >= 0 && matrix[row][col] > mid){
+                    col--;
+                }
+
+                count += col + 1;
+            }
+
+            if(count < k){
+                l = mid + 1;
+            }
+            else{
+                r = mid - 1;
+            }
+        }
+
+        return l;
+    }
+}
