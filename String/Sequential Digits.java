d70b2a1d61a3d8b7b40c656da168d7bb6a98c887
Raj Jaiswal <143874457+Rajj23@users.noreply.github.com>
2026-07-13 23:18:19 +0530

    Sequential Digits

---
+++ String/Sequential Digits.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> result = new ArrayList<>();
+
+        int n = String.valueOf(low).length();
+        int m = String.valueOf(high).length();
+
+        String digit = "123456789";
+
+        for(int len = n; len <= m; len++){
+            
+            for(int i = 0; i + len <= digit.length(); i++) {
+                int value = Integer.parseInt(digit.substring(i, i + len));
+
+                if(value >= low && value <= high){                    
+                    result.add(value);
+                }
+            }
+
+        }
+
+        return result;
+    }
+}
